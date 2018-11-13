4160b995bd48ce8bf0509cc368960bf2dc97635e
Kai Schmidt <kaikaliischmidt@gmail.com>
2018-11-12 16:58:40 -0800

    crate is looking good

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,125 @@
 #![deny(missing_docs, unsafe_code)]
 
-//! This crate provides a veriety of timer types for measuring time in a program in different ways.
+//! This crate provides timer types for measuring time in a program in different ways.
+//! - `Timer` is a timer that counts up and knows how much time has passed since it was started.
+//! - `EggTimer` is a timer that counts down from its set `Duration` and knows how much time it has left.
+//! - `Stopwatch` is a timer that counts up and can be paused and resumed.
+//!
+//! In addition to the timer types, a collection type, `TimedList`, is provided,
+//! which associates each element with a `Duration` and only retains elements whose `Duration` has not elapsed
 
-use std::{
-    thread::{self, JoinHandle},
-    time::{Duration, Instant},
-};
+use std::time::{Duration, Instant};
 
-/// Converts a floating-point time value in seconds to a `Duration`
-pub fn seconds_to_duration(seconds: f64) -> Duration {
-    let whole = seconds as u64;
-    let fract = (seconds.fract() * 1e9) as u32;
-    Duration::new(whole, fract)
+/// A trait for types that can be turned into a `Duration`
+pub trait ToDuration {
+    /// Convert the value into the `Duration`
+    fn to_duration(&self) -> Duration;
 }
 
-/// Converts a `Duration` to a floating-point time value
-pub fn duration_to_seconds(duration: Duration) -> f64 {
-    duration.as_secs() as f64 + f64::from(duration.subsec_nanos()) / 1e9
+impl ToDuration for Duration {
+    fn to_duration(&self) -> Duration {
+        *self
+    }
+}
+
+impl ToDuration for f32 {
+    fn to_duration(&self) -> Duration {
+        if self < &0.0 {
+            panic!("Attempted to convert negative floating-point number to Duration");
+        }
+        let whole = *self as u64;
+        let fract = (self.fract() * 1e9) as u32;
+        Duration::new(whole, fract)
+    }
+}
+
+impl ToDuration for f64 {
+    fn to_duration(&self) -> Duration {
+        if self < &0.0 {
+            panic!("Attempted to convert negative floating-point number to Duration");
+        }
+        let whole = *self as u64;
+        let fract = (self.fract() * 1e9) as u32;
+        Duration::new(whole, fract)
+    }
+}
+
+impl ToDuration for u8 {
+    fn to_duration(&self) -> Duration {
+        Duration::new(*self as u64, 0)
+    }
+}
+
+impl ToDuration for u16 {
+    fn to_duration(&self) -> Duration {
+        Duration::new(*self as u64, 0)
+    }
+}
+
+impl ToDuration for u32 {
+    fn to_duration(&self) -> Duration {
+        Duration::new(*self as u64, 0)
+    }
+}
+
+impl ToDuration for u64 {
+    fn to_duration(&self) -> Duration {
+        Duration::new(*self, 0)
+    }
+}
+
+impl ToDuration for u128 {
+    fn to_duration(&self) -> Duration {
+        Duration::new(*self as u64, 0)
+    }
+}
+
+impl ToDuration for usize {
+    fn to_duration(&self) -> Duration {
+        Duration::new(*self as u64, 0)
+    }
+}
+
+/// A trait for types that can be created from a `Duration`
+pub trait FromDuration {
+    /// Create the value from a `Duration`
+    fn from_duration(duration: Duration) -> Self;
+}
+
+impl FromDuration for Duration {
+    fn from_duration(duration: Duration) -> Self {
+        duration
+    }
+}
+
+impl FromDuration for u64 {
+    fn from_duration(duration: Duration) -> Self {
+        duration.as_secs()
+    }
+}
+
+impl FromDuration for u128 {
+    fn from_duration(duration: Duration) -> Self {
+        duration.as_secs() as u128
+    }
+}
+
+impl FromDuration for usize {
+    fn from_duration(duration: Duration) -> Self {
+        duration.as_secs() as usize
+    }
+}
+
+impl FromDuration for f32 {
+    fn from_duration(duration: Duration) -> Self {
+        duration.as_secs() as f32 + duration.subsec_nanos() as f32 / 1e9
+    }
+}
+
+impl FromDuration for f64 {
+    fn from_duration(duration: Duration) -> Self {
+        duration.as_secs() as f64 + f64::from(duration.subsec_nanos()) / 1e9
+    }
 }
 
 /// A simple timer that knows how long since it started
@@ -36,10 +139,17 @@ impl Timer {
     pub fn reset(&mut self) {
         self.start = Instant::now();
     }
-    /// Gets the elapsed time in seconds
-    pub fn elapsed(self) -> f64 {
-        let duration = Instant::now().duration_since(self.start);
-        duration_to_seconds(duration)
+    /// Gets the elapsed time as a floating-point number of seconds
+    pub fn elapsed(&self) -> f64 {
+        f64::from_duration(self.duration())
+    }
+    /// Get the elapsed time as a `Duration`
+    pub fn duration(&self) -> Duration {
+        Instant::now().duration_since(self.start)
+    }
+    /// Gets the `Instant` at which the `Timer` was started
+    pub fn started_at(&self) -> Instant {
+        self.start
     }
 }
 
@@ -49,88 +159,135 @@ impl Default for Timer {
     }
 }
 
-/// A timer that counts down
-#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+/// A timer that counts down and knows when a `Duration` has elapsed
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct EggTimer {
     timer: Timer,
-    dur: f64,
+    duration: Duration,
 }
 
 impl EggTimer {
     /// Creates a new `EggTimer`
-    pub fn set(seconds: f64) -> EggTimer {
+    pub fn set<D: ToDuration>(time: D) -> EggTimer {
         EggTimer {
             timer: Timer::start(),
-            dur: seconds,
+            duration: time.to_duration(),
         }
     }
     /// Resets the `EggTimer`
     pub fn reset(&mut self) {
         self.timer = Timer::start();
     }
-    /// Gets the time left in seconds
-    pub fn time_left(self) -> f64 {
-        self.dur - self.timer.elapsed()
+    /// Gets the time left as a `Duration`
+    pub fn duration_left(&self) -> Option<Duration> {
+        self.duration.checked_sub(self.timer.duration())
     }
-    /// Checks if the timer is ready
+    /// Gets the time left as a floating-point number of seconds
+    pub fn seconds_left(&self) -> f64 {
+        f64::from_duration(self.duration) - self.timer.elapsed()
+    }
+    /// Checks if the set `Duration` has elapsed
     pub fn is_ready(self) -> bool {
-        self.time_left() <= 0.0
+        self.duration_left().is_none()
     }
-    /// Gets the number of seconds the `EggTimer` was originally set with
-    pub fn max_time(&self) -> f64 {
-        self.dur
+    /// Gets the time the `EggTimer` was originally set with as a `Duration`
+    pub fn max_duration(&self) -> Duration {
+        self.duration
+    }
+    /// Gets the time the `EggTimer` was originally set with as a floating-point number of seconds
+    pub fn max_seconds(&self) -> f64 {
+        f64::from_duration(self.max_duration())
+    }
+    /// Gets the `Instant` at which the `EggTimer` was started
+    pub fn started_at(&self) -> Instant {
+        self.timer.started_at()
+    }
+    /// Gets the `Instant` at which the `EggTimer` will or did end
+    pub fn ends_at(&self) -> Instant {
+        self.timer.started_at() + self.duration
     }
 }
 
-/// An alarm that calls a function when it is ready
-pub struct Alarm<T>
-where
-    T: Send + 'static,
-{
-    handle: JoinHandle<T>,
-    timer: EggTimer,
+/// A timer that can be paused and resumed.
+///
+/// The reported elapsed times do not include periods when the timer was paused
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Stopwatch {
+    last_start: Instant,
+    prev_dur: Duration,
+    paused: bool,
 }
 
-impl<T> Alarm<T>
-where
-    T: Send + 'static,
-{
-    /// Creates a new `Alarm`
-    pub fn set<F>(seconds: f64, alarm: F) -> Alarm<T>
-    where
-        F: FnOnce() -> T + Send + 'static,
-    {
-        let timer = EggTimer::set(seconds);
-        Alarm {
-            handle: thread::spawn(move || {
-                thread::sleep(seconds_to_duration(if seconds < 0.12 {
-                    seconds * 0.9
-                } else {
-                    seconds - 0.1
-                }));
-                while !timer.is_ready() {}
-                alarm()
-            }),
-            timer,
+impl Stopwatch {
+    /// Creates a new `Stopwatch` which immediately starts counting
+    pub fn start() -> Stopwatch {
+        Stopwatch {
+            last_start: Instant::now(),
+            prev_dur: 0u64.to_duration(),
+            paused: false,
         }
     }
-    /// Gets the time left in seconds
-    pub fn time_left(&self) -> f64 {
-        self.timer.time_left()
+    /// Creates a new `Stopwatch` which starts paused
+    pub fn start_paused() -> Stopwatch {
+        Stopwatch {
+            last_start: Instant::now(),
+            prev_dur: 0u64.to_duration(),
+            paused: true,
+        }
     }
-    /// Checks if the timer is ready
-    pub fn is_ready(&self) -> bool {
-        self.timer.is_ready()
+    /// Restarts the `Stopwatch` without pausing or resuming
+    pub fn reset(&mut self) {
+        self.last_start = Instant::now();
+        self.prev_dur = 0u64.to_duration();
     }
-    /// Blocks the current thread until the `Alarm` goes off
-    pub fn join(self) -> thread::Result<T> {
-        self.handle.join()
+    /// Gets the elapsed time as a floating-point number of seconds
+    pub fn elapsed(&self) -> f64 {
+        f64::from_duration(self.duration())
+    }
+    /// Gets the elapsed time as a `Duration`
+    pub fn duration(&self) -> Duration {
+        if self.paused {
+            self.prev_dur
+        } else {
+            self.prev_dur + Instant::now().duration_since(self.last_start)
+        }
+    }
+    /// Pauses the `Stopwatch`
+    pub fn pause(&mut self) {
+        if !self.paused {
+            self.prev_dur += Instant::now().duration_since(self.last_start);
+        }
+    }
+    /// Resumes the `Stopwatch`
+    pub fn resume(&mut self) {
+        if self.paused {
+            self.last_start = Instant::now();
+        }
+    }
+    /// Toggles whether the `Stopwatch` is paused or resumed
+    pub fn toggle(&mut self) {
+        if self.paused {
+            self.pause();
+        } else {
+            self.resume();
+        }
+    }
+    /// Gets the `Instant` at which the `Stopwatch` was last resumed
+    pub fn started_at(&self) -> Instant {
+        self.last_start
     }
 }
 
-/// An iterable list structure where each element has an associated duration.
-/// When an element's duration has elapsed, the element is removed from the
-/// list upon the next mutable function call.
+impl Default for Stopwatch {
+    fn default() -> Self {
+        Stopwatch::start()
+    }
+}
+
+/// An iterable list structure where each element has an associated `Duration`.
+///
+/// When an element's `Duration` has elapsed, the element is removed from the
+/// list upon the next mutable function call. Timed-out elements will never be iterated over.
 #[derive(Debug, Clone, PartialEq, PartialOrd, Default)]
 pub struct TimedList<T> {
     list: Vec<(EggTimer, T)>,
@@ -141,12 +298,13 @@ impl<T> TimedList<T> {
     pub fn new() -> TimedList<T> {
         TimedList { list: Vec::new() }
     }
-    /// Inserts and element into the list with the given duration
-    pub fn insert(&mut self, element: T, duration: f64) {
-        self.list.push((EggTimer::set(duration), element));
+    /// Inserts and element into the list with the given number of floating-point seconds
+    pub fn insert<D: ToDuration>(&mut self, element: T, time: D) {
+        self.list.push((EggTimer::set(time), element));
     }
-    /// Forces the removal of all elements whose duration has elpased.
-    /// This method does not need to be called manually.
+    /// Forces the removal of all elements whose `Duration` has elpased.
+    /// This method does not need to be called manually unless you
+    /// want to explicitely free the memory of timed-out elements immediately.
     pub fn clean(&mut self) {
         self.list.retain(|(timer, _)| !timer.is_ready());
     }
@@ -154,7 +312,12 @@ impl<T> TimedList<T> {
     pub fn clear(&mut self) {
         self.list.clear();
     }
+    /// Gets the number of elements in the list that have not timed out.
+    pub fn len(&self) -> usize {
+        self.iter().count()
+    }
     /// Iterates immutably through all elements.
+    ///
     /// While this method does not remove timed-out elements,
     /// it does filter them out.
     /// If iteration takes sufficiently long, elements that
@@ -172,6 +335,7 @@ impl<T> TimedList<T> {
         )
     }
     /// Iterates mutably through all elements.
+    ///
     /// If iteration takes sufficiently long, elements that
     /// may have been valid when iteration began may be skipped
     /// when they are atually iterated over.
@@ -186,6 +350,37 @@ impl<T> TimedList<T> {
                 }
             },
         )
+    }
+    /// Iterates immutably through all elements and their timers.
+    ///
+    /// While this method does not remove timed-out elements,
+    /// it does filter them out.
+    /// If iteration takes sufficiently long, elements that
+    /// may have been valid when iteration began may be skipped
+    /// when they are atually iterated over.
+    pub fn timer_iter(&self) -> impl Iterator<Item = (&T, EggTimer)> {
+        self.list.iter().filter_map(|(timer, elem)| {
+            if timer.is_ready() {
+                None
+            } else {
+                Some((elem, *timer))
+            }
+        })
+    }
+    /// Iterates mutably through all elements.
+    ///
+    /// If iteration takes sufficiently long, elements that
+    /// may have been valid when iteration began may be skipped
+    /// when they are atually iterated over.
+    pub fn timer_iter_mut(&mut self) -> impl Iterator<Item = (&mut T, EggTimer)> {
+        self.clean();
+        self.list.iter_mut().filter_map(|(timer, elem)| {
+            if timer.is_ready() {
+                None
+            } else {
+                Some((elem, *timer))
+            }
+        })
     }
 }
 
